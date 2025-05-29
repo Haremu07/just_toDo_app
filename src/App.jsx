0476d281414a0ee1e3e0732911bc3aa8cd89c8d4
Taofik Ahmed <tafikahmed007@example.com>
2025-05-29 12:51:58 +0100

    edit fuction added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useReducer } from 'react'
-import { MdDelete } from 'react-icons/md';
+import React, { useEffect, useReducer } from "react";
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
 
 const App = () => {
   const initialState = {
     input: "",
-    tasks: JSON.parse(localStorage.getItem("tasks")) ||  [],
+    tasks: JSON.parse(localStorage.getItem("tasks")) || [],
+    editingId: null,
   };
-
 
   const reducer = (state, action) => {
     switch (action.type) {
@@ -15,6 +16,7 @@ const App = () => {
           ...state,
           input: action.payload,
         };
+
       case "addTask":
         if (!state.input.trim()) return state;
         const newTask = {
@@ -22,21 +24,38 @@ const App = () => {
           text: state.input.trim(),
         };
 
-         return {
+        return {
           ...state,
           tasks: [...state.tasks, newTask],
           input: "",
         };
 
-        case "delete":
-          return {
-            ...state,
-            tasks: state.tasks.filter((task) => task.id != action.payload),
-          }
+      case "startEdit":
+        const taskToEdit = state.tasks.find(
+          (task) => task.id === action.payload
+        );
+        return {
+          ...state,
+          input: taskToEdit?.text,
+          editingId: action.payload,
+        };
 
-          break
+      case "applyEdit":
+        return {
+          ...state,
+          tasks: state.tasks.map((task) =>
+            task.id === state.editingId ? { ...task, text: state.input } : task
+          ),
+          input: "",
+          editingId: null,
+        };
 
-       
+      case "delete":
+        return {
+          ...state,
+          tasks: state.tasks.filter((task) => task.id !== action.payload),
+        };
+
       default:
         return state;
     }
@@ -52,12 +71,20 @@ const App = () => {
   };
 
   const handleTask = () => {
-    dispatch({ type: "addTask" });
+    if (state.editingId) {
+      dispatch({ type: "applyEdit" });
+    } else {
+      dispatch({ type: "addTask" });
+    }
   };
 
   const handleDelete = (id) => {
-    dispatch({type: "delete", payload: id})
-  } 
+    dispatch({ type: "delete", payload: id });
+  };
+
+  const handleEdit = (id) => {
+    dispatch({ type: "startEdit", payload: id });
+  };
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(state.tasks));
@@ -65,38 +92,53 @@ const App = () => {
 
   return (
     <div className="h-screen bg-black flex justify-center items-center">
-    <div className="w-80 rounded-2xl p-3 bg-gray-300 flex flex-col gap-3">
-      <h1 className="text-center text-lg font-bold">To-Do App</h1>
-      <div className="w-full bg-white h-10 pl-3 flex p-1">
-        <input
-          type="text"
-          value={state.input}
-          onChange={handleChange}
-          placeholder="Add Todo"
-          className="w-full outline-none"
-        />
-        <button
-          onClick={handleTask}
-          className="bg-black text-white w-20 h-full cursor-pointer"
-        >
-          Add
-        </button>
-      </div>
-      {state.tasks.map((task) => (
-        <div
-          key={task.id}
-          className="w-full h-10 bg-white flex pl-1 items-center justify-between"
-        >
-          <p>{task.text}</p>
-          <div className="w-12 h-full flex items-center gap-2">
-            {/* <MdEdit className="cursor-pointer" /> */}
-            <MdDelete onClick={() => handleDelete(task.id)} className="cursor-pointer" />
-          </div>
+      <div className="w-80 max-h-full rounded-2xl p-3 bg-gray-300 flex flex-col gap-3">
+        <h1 className="text-center text-lg font-bold">To-Do App</h1>
+
+        <div className="w-full bg-white h-10 pl-3 flex p-1">
+          <input
+            type="text"
+            value={state.input}
+            onChange={handleChange}
+            placeholder="Add  Todo"
+            className="w-full outline-none"
+          />
+          <button
+            onClick={handleTask}
+            className="bg-black text-white w-20 h-full cursor-pointer"
+          >
+            {state.editingId ? "Save" : "Add"}
+          </button>
         </div>
-      ))}
+
+        <div className="overflow-y-auto flex flex-col gap-2">
+          {state.tasks.map((task) => (
+            <div
+              key={task.id}
+              className="w-full h-10 bg-white flex pl-1 items-center justify-between"
+            >
+              <p>{task.text}</p>
+              <div className="w-12 h-full flex items-center gap-2">
+                <MdEdit
+                  onClick={() => handleEdit(task.id)}
+                  className="cursor-pointer text-green-500"
+                />
+                <MdDelete
+                  onClick={() => {
+                    let confirm = window.confirm(
+                      "Are you sure you want to delete this task?"
+                    );
+                    if (confirm) handleDelete(task.id);
+                  }}
+                  className="cursor-pointer text-red-500"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
-export default App
+export default App;
